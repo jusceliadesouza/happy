fc72f2bbbf9a47ab97f8b1b4b208d6872e218f72
Juscélia de Souza <jusceliadesousa@gmail.com>
2020-10-17 20:12:01 -0300

    Dia01: estrutura web

---
+++ web/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch> {/*Apenas uma única rota é chamada*/}
+                <Route path="/" exact component={Landing} />
+                <Route path="/app" component={OrphanagesMap} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
